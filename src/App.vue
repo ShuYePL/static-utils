23dcb8538f440a55b5e599a3b8d12b0db6da2e59
ShuYePL <3548768596@qq.com>
2024-11-16 23:42:40 +0800

    jsonView过长时出现滚动条

--- src/App.vue
+++ src/App.vue
@@ -9,4 +9,33 @@ import JsonUtil from './components/JsonUtil.vue'
 </template>
 
 <style scoped>
+.el-container {
+  height: 100%;
+}
+
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+.el-menu {
+  background-color: #d3dce6;
+}
 </style>
--- src/components/JsonUtil.vue
+++ src/components/JsonUtil.vue
@@ -10,37 +10,80 @@ var copyShowText = {copyText: '复制', copiedText: '已复制'}
 var data = reactive({
   jsonObj: null
 })
+// log模式下的json对象列表
+var jsonObjList = ref([])
 // 当前json字符串
 var jsonStr = ref("")
 // 是否展示转换后的字符串
 var isShowJsonViewer = ref(true)
+// 是否进行字符过滤，过滤斜杠和换行符等空格符号
+var isFilterSpecialChar = ref(false)
+var isShowJsonViewer = ref(false)
+var isReadLog = ref(false)
+
+/**
+ * 判断字符串是否不为空
+ * 
+ * @param jsonStr 待判断字符串
+ */
+const isNotEmptyStr = (jsonStr) => {
+  return !isEmpty(jsonStr);
+}
+
+/**
+ * 判断字符串是否为空
+ * 
+ * @param jsonStr 待判断字符串
+ */
+const isEmptyStr = (jsonStr) => {
+  return jsonStr.value.trim() == "";
+}
 
 /**
  * 将json字符串转换为json对象
  */
 const parseJson = () => {
-  console.log("jsonStr:", jsonStr.value)
-  if (jsonStr.value !== "" && jsonStr !== '') {
-    jsonStr.value = jsonStr.value.replace(/[/|\\|*]*/g, "")
-    console.log("after JsonStr replace:", jsonStr.value)
-    data.jsonObj = JSON.parse(jsonStr.value)
-    console.log("jsonObj parse")
-  } else {
-    data.jsonObj = null
-    console.log("jsonObj not parse")
+  // 字符串为空，不做处理
+  if (isEmptyStr(jsonStr)) {
+    data.jsonObj = {};
+    return;
   }
-  console.log("jsonObj:", data.jsonObj);
+  
+  // // 判断是否为log模式
+  // if (isReadLog) {
+  //   // 使用换行符来分割JSON字符串
+  //   var jsonUnits = jsonStrings.split(/\n+/);
+
+  //   // 解析每个JSON字符串为JavaScript对象
+  //   var jsonObjList = jsonUnits.map(json => {
+  //     try {
+  //       return JSON.parse(json);
+  //     } catch (error) {
+  //       console.error('Failed to parse JSON:', error);
+  //       return [];
+  //     }
+  //   });
+  // }
+
+  // 字符串非空
+  if (isFilterSpecialChar === true) {
+    var tempJsonStr = jsonStr.value.replace(/[/|\\|\n|\r|\t"]*/g, "")
+  } else {
+    var tempJsonStr = jsonStr.value
+  }
+  console.log("after JsonStr replace:", tempJsonStr)
+  data.jsonObj = JSON.parse(tempJsonStr)
 }
 
 /**
  * 判断jsonStr是否为空，如果为空，则不用放置json-viewer
  */
- const judgeIsShowJsonViewer = () => {
+const judgeIsShowJsonViewer = () => {
   console.log("判断jsonStr是否为空, jsonStr", jsonStr)
   if (jsonStr.value === "" || jsonStr.value === '') {
-    // isShowJsonViewer = false;
+    isShowJsonViewer = false;
   } else {
-    // isShowJsonViewer = true;
+    isShowJsonViewer = true;
   }
   console.log("isShowJsonViewer:", isShowJsonViewer);
 }
@@ -57,14 +100,20 @@ const parseJsonStr2Obj = () => {
   
 <template>    
   <div class="frame">
-    <el-input type="textarea" rows=100 v-model="jsonStr" placeholder="在这里输入json字符串 ~ ~ ~"
+    <el-input type="textarea" v-model="jsonStr" placeholder="在这里输入json字符串 ~ ~ ~"
       input-style="width: 100%; height: 100vh; background-color: #eee;"></el-input>
     <div class="mid-bar">
       <el-button @click="parseJsonStr2Obj" size="small" type="primary" :icon="ArrowRight">parse</el-button>
-    </div>
-    <div class="json-view-area">
+      <br>
+      <el-switch v-model="isFilterSpecialChar" size="large"
+        active-text="filter" inactive-text="filter" inline-prompt></el-switch>
+      <!-- <el-switch v-model="isReadLog" size="large"
+        active-text="log" inactive-text="log" inline-prompt></el-switch> -->
+      </div>
+    <div class="json-view-area" v-show="isShowJsonViewer">
       <json-viewer :value="data.jsonObj" :expand-depth=5 :copyable="copyShowText" expanded="true"></json-viewer>
     </div>
+    <div class="json-view-area" v-show="!isShowJsonViewer"></div>
   </div>
 </template>
 
@@ -77,7 +126,7 @@ const parseJsonStr2Obj = () => {
 
   .mid-bar {
     width: 8%;
-    background-color: #ffe4c4;
+    background-color: #fef8f0;
     display: flex;
     flex-direction: column;
     justify-content: center;  // 水平居中
@@ -88,6 +137,7 @@ const parseJsonStr2Obj = () => {
 .json-view-area {
   width: 98%;
   height: 100vh;
+  overflow: auto;
 }
 
 </style>
